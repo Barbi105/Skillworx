4e58ba8cbdc55b1393638adfaf601dc8162ec521
Ulises Rangel <uli.atx@gmail.com>
2019-05-03 13:26:10 -0500

    removed handlebars, added some component templates, added bootstrap and fontawesome cdn

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Project 3. Full MERN
+        </h1>
+        <h4>
+          Kat, Des, Barb, Uli
+        </h4>
+      </div>
+    )
+  }
+}
+
+export default Main

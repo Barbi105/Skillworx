28fb13c35fa5c1148ee9ed245392dd3911484d82
Ulises Rangel <uli.atx@gmail.com>
2019-05-07 15:07:59 -0500

    resolved merge

--- app.js
+++ app.js
@@ -3,23 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-<<<<<<< Updated upstream
 var mongoose = require('mongoose');
 
 var app = express();
 var routes = require('./routes');
-=======
 var PORT = process.env.PORT || 3001;
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
-var app = express();
-
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
->>>>>>> Stashed changes
 
 app.use(logger('dev'));
 app.use(express.json());

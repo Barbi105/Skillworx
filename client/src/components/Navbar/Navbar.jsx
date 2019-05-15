d706e5e22ffbd790caeafc373abe81a0364e9053
barbi105 <bontiverosg@gmail.com>
2019-05-14 23:49:11 -0500

    logo edit size

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
     <nav className="navbar navbar-light notLanding" >
       <span className="navbar-brand mb-0">
         <span className="cap-brand">S</span>
-        <span className="small-cap-brand">kill</span>
+        <span className="small-cap-brand kill">kill</span>
         <span className="cap-brand w">W</span>
         <span className="small-cap-brand">orx</span>
       </span>
